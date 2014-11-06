feacb2e4fc8202bdbdd7c5f3944d1e1f0d54990b
Frederick Wilson <wilsonfl@g.cofc.edu>
2014-11-05 22:23:31 -0500

    Initial commit- first ever to github

---
+++ testClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Frederick Wilson on 11/5/2014.
+ */
+public class testClass {
+    public static void main(String[] args) {
+    System.out.println("Hello world, I now have a github account.....the world should be scared");
+    }
+}
