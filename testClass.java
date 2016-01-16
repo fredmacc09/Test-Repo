80dc4b92886dcbc09072987887c1f69bd644ff85
fredmacc09 <wilsonfl@g.cofc.edu>
2016-01-15 19:31:09 -0500

    adding edited testClass files to my personal repository

--- testClass.java
+++ testClass.java
@@ -2,7 +2,16 @@
  * Created by Frederick Wilson on 11/5/2014.
  */
 public class testClass {
+
     public static void main(String[] args) {
+
+String[] names = {"Freddy","Katrina","Phil","Bob"};
+
     System.out.println("Hello world, I now have a github account.....the world should be scared");
+
+	for(int i=0;i<names.length;i++){
+		System.out.println(names[i]);
+
+	}
     }
 }
